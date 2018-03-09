097eb55b3b389733332ca0e140758f1dd1fccf9a
coder-pig <51181090@qq.com>
2018-03-09 23:19:09 +0800

    update

--- socket/pages/share/share.js
+++ socket/pages/share/share.js
@@ -5,6 +5,8 @@ const did = wx.getStorageSync('didJSon').did;
 var tools = require('../../utils/util.js');
 import { $ } from '../../utils/main.js'
 
+var QR = require("../../utils/qrcode.js");
+
 Page({
 
   /**
@@ -26,6 +28,7 @@ Page({
       'X-Gizwits-User-token': wx.getStorageSync('options').token,
     },
     devices: [],
+    canvasHidden: false,
   },
 
   /**
@@ -41,6 +44,7 @@ Page({
       success(res) {
         that.setData({
           Htop: (res.windowHeight - 250) / 2,
+          winTop: (res.windowHeight - 310) / 2,
         });
       },
     });
@@ -125,6 +129,7 @@ Page({
   addShare() {
     let that = this;
     let regMobile = /^1[3|4|5|8][0-9]\d{4,8}$/;
+    var size = that.setCanvasSize();
     // if (that.data.phoneText == '') {
     //   wx.showToast({
     //     title: '请输入手机号码！！',
@@ -143,7 +148,11 @@ Page({
       'X-Gizwits-User-token': options.token,
     };
     let wxuser = wx.getStorageSync('wxuser');
-
+    wx.showToast({
+      title: '生成中...',
+      icon: 'loading',
+      duration: 2000
+    });
     wx.request({
       url: 'https://api.gizwits.com/app/sharing',
       method: "POST",
@@ -155,6 +164,7 @@ Page({
         "duration": 1
       },
       success(res) {
+        wx.hideToast();
         if (res.data.error_code == 9081) {
           wx.showModal({
             title: '警告!',
@@ -171,7 +181,8 @@ Page({
           return false;
         }
         let code = res.data.qr_content;
-        code = code.substring(16, 48)
+        code = code.substring(16, 48);
+        that.createQrCode(code, "mycanvas", size.w, size.h);
         //  创建设备分享
         // wx.request({
         //   url: 'https://api.gizwits.com/app/sharing/code/' + code,
@@ -193,7 +204,7 @@ Page({
         //   },
         // })
         that.setData({
-          layer: true,
+          layer: false,
         });
       }
     })
@@ -210,6 +221,57 @@ Page({
     this.setData({
       phoneText: e.detail.value
     });
+  },
+
+  //适配不同屏幕大小的canvas
+  setCanvasSize: function () {
+    var size = {};
+    try {
+      var res = wx.getSystemInfoSync();
+      var scale = 930 / 700;//不同屏幕下canvas的适配比例；设计稿是750宽
+      var width = res.windowWidth / scale;
+      var height = width;//canvas画布为正方形
+      size.w = width;
+      size.h = height;
+    } catch (e) {
+      // Do something when catch error
+      console.log("获取设备信息失败" + e);
+    }
+    return size;
+  },
+  createQrCode: function (url, canvasId, cavW, cavH) {
+    //调用插件中的draw方法，绘制二维码图片
+    QR.api.draw(url, canvasId, cavW, cavH);
+    setTimeout(() => { this.canvasToTempImage(); }, 1000);
+  },
+
+  //获取临时缓存照片路径，存入data中
+  canvasToTempImage: function () {
+    var that = this;
+    wx.canvasToTempFilePath({
+      canvasId: 'mycanvas',
+      success: function (res) {
+        var tempFilePath = res.tempFilePath;
+        console.log(tempFilePath);
+        that.setData({
+          imagePath: tempFilePath,
+          // canvasHidden:true
+        });
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    });
+  },
+
+  //点击图片进行预览，长按保存分享图片
+  previewImg: function (e) {
+    var img = this.data.imagePath;
+    console.log(img);
+    wx.previewImage({
+      current: img, // 当前显示图片的http链接
+      urls: [img] // 需要预览的图片http链接列表
+    })
   },
 
 })
